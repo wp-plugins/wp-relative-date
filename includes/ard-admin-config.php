a79caebc19b7e5c648e09f53137cd47a0aefd890
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-12 07:29:39 +0000

    Adding 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-relative-date/trunk@1197352 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/ard-admin-config.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* Admin Menu Functions
+*
+* Various functions relating to the various administration screens
+*
+* @package	Artiss-Relative-Date
+*/
+
+/**
+* Add meta to plugin details
+*
+* Add options to plugin meta line
+*
+* @since	1.2
+*
+* @param	string  $links	Current links
+* @param	string  $file	File in use
+* @return   string			Links, now with settings added
+*/
+
+function ard_set_plugin_meta( $links, $file ) {
+
+	if ( strpos( $file, 'wp-relative-date.php' ) !== false ) {
+		$links = array_merge( $links, array( '<a href="https://wordpress.org/support/plugin/wp-relative-date">' . __( 'Support', 'wp-relative-date' ) . '</a>' ) );
+		$links = array_merge( $links, array( '<a href="http://www.artiss.co.uk/donate">' . __( 'Donate', 'wp-relative-date' ) . '</a>' ) );
+	}
+
+	return $links;
+}
+
+add_filter( 'plugin_row_meta', 'ard_set_plugin_meta', 10, 2 );
+?>
